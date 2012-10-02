c2a60c1509dcbe25fc80fb57da062054223c1cf5
Jun Zhang <zhangjun1126@gmail.com>
2012-10-02 10:34:57 +0800

    minor modification

--- demoDoRelease.m
+++ demoDoRelease.m
@@ -20,7 +20,7 @@ end
 %% --------------------------------------------------------------
 %           load C1DO prototypes if exsits or extract your own prototypes
 % -------------------------------------------------------------------------
-READPATCHESFROMFILE = 1;
+READPATCHESFROMFILE = 0;
 patchSizes = [4 8 12 16];
 numPatchSizes = length(patchSizes);
 numPatchesPerSize = 250;
